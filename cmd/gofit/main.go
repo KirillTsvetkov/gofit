1ee3ab47b0d7d4624f74bffeb8bc27648cc3d262
Kiril Tsvetkov <kirill.tsvetkov31@gmail.com>
2024-02-24 22:54:11 +0300

    add viper and init configs

--- cmd/gofit/main.go
+++ cmd/gofit/main.go
@@ -4,13 +4,19 @@ import (
 	"log"
 
 	"github.com/KirillTsvetkov/gofit"
-	"github.com/KirillTsvetkov/gofit/pkg/handler"
+	"github.com/KirillTsvetkov/gofit/config"
+	"github.com/KirillTsvetkov/gofit/handler"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := config.Init(); err != nil {
+		log.Fatalf("%s", err.Error())
+	}
+
 	handeler := new(handler.Handler)
 	srv := new(gofit.Server)
-	if err := srv.Run("8080", handeler.IniteRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handeler.IniteRoutes()); err != nil {
 		log.Fatalf("error: %s", err.Error())
 	}
 }
