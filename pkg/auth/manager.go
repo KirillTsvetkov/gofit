aaf8dcd3bfa0f370ae06ae0070fe79f68f53a061
Kiril Tsvetkov <kirill.tsvetkov31@gmail.com>
2024-03-17 00:20:34 +0300

    fix auth manager

--- pkg/auth/manager.go
+++ pkg/auth/manager.go
@@ -22,8 +22,8 @@ func NewManager(jwtKey string) (*Manager, error) {
 
 func (m *Manager) GenerateJWT(userID string, ttl time.Duration) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"user_id": userID,
-		"exp":     time.Now().Add(ttl).Unix(),
+		"sub": userID,
+		"exp": time.Now().Add(ttl).Unix(),
 	})
 
 	tokenString, err := token.SignedString([]byte(m.jwtKey))
@@ -39,7 +39,7 @@ func (m *Manager) ValidateJWT(tokenString string) (string, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
-		return m.jwtKey, nil
+		return []byte(m.jwtKey), nil
 	})
 
 	if err != nil {
